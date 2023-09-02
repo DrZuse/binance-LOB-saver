4394dcf5a3d8aa0a0eb7ddc741fad4320ba5c22d
DrZuse <53859830+DrZuse@users.noreply.github.com>
2023-09-02 12:40:27 +0300

    clean

--- test.py
+++
@@ -1,3 +0,0 @@
-with open("sample.txt", "a") as file_object:
-    # Append 'hello' at the end of file
-    file_object.write("hello")
--- uni.py
+++
@@ -1,4 +0,0 @@
-import unicorn_binance_local_depth_cache
-
-ubldc = unicorn_binance_local_depth_cache.BinanceLocalDepthCacheManager(exchange="binance.com")
-ubldc.create_depth_cache("BTCUSDT")
